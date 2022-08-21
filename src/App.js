d22a3ff8ca0c742bdc787846feae29db1b2e3332
Maria Carolina <demycar24@gmail.com>
2022-08-20 21:41:36 -0300

    alteração

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const FilmesApi = axios.create({
+    baseURL:'https://api.themoviedb.org/3/movie/popular?api_key=a5e04285a5e5085c51d1ae9fb4ccbccf&language=en-US&page=1'
+})
+
+
+class Movies extends Component {
+state={
+    movies:[]
 }
 
-export default App;
+componentDidMount(){
+    this.getMovies()
+}
+
+getMovies = async () => {
+    const resposta = await FilmesApi.get()
+    
+
+    const allFilmes = resposta.data.results.map((item) => {
+        return{
+          ...item,
+          nome:item.original_title
+        }
+    })
+    this.setState({
+        movies:allFilmes
+    })
+    console.log(this.state.movies)
+}
+
+    render() {
+        return (
+            <div>
+                {this.state.movies.map((item) => (
+                    <ul>
+                        <li>{item.nome}</li>
+                        <li>{item.overview}</li>
+                    </ul>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Movies;
